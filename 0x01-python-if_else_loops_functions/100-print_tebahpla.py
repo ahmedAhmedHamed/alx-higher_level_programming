956a44cc630e9e585c6cd1151f51d68dcddfc19a
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-05-09 21:07:51 +0300

    (100)

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 j = 0
-for i in range(52):
+for i in range(25, -1, -1):
     k = 0
     if i % 2 == 0:
-        k = int(i / 2) + 65
+        k = i + 65
     else:
-        k = int(i / 2) + 97
+        k = i + 97
     print("{}".format(chr(k)), end="")
