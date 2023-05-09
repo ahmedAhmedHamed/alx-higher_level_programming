53833617907128dba8fcf498e527c28780d3acf7
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-05-09 18:19:46 +0300

    (12)

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        flag = 0
+        if i % 3 == 0:
+            flag = 1
+            print("fizz", end="")
+        if i % 5 == 0:
+            flag = 1
+            print("buzz", end="")
+        if flag:
+            if i != 100:
+                print(" ", end="")
+            else:
+                print("")
+            continue
+        print(f"{i} ", end="")
+    print("")
