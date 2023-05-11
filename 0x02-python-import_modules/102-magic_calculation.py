8ade5d1fb1f5b426cd141f86e42342a586e0a03f
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-05-11 18:57:12 +0300

    (102)

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -6,4 +6,8 @@ def magic_calculation(a, b):
         for i in range(3, 4, 6):
             c = add(c, i)
         return c
-    return sub(a, b)
+
+    sub(a, b)
+
+import dis
+dis.dis(magic_calculation)
