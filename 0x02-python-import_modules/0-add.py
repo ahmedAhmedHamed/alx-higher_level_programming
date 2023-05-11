de1d2e739d2090e97a552eafb10e2f5a2b39f467
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-05-11 13:00:24 +0300

    (0)

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,13 +2,11 @@
 import add_0 as ewew
 
 
-def add(a, b):
-    print("{} + {} = {}".format(a, b, ewew.add(a, b)))
-
-
 a = 1
 b = 2
-add(a, b)
+
+print("{} + {} = {}".format(a, b, ewew.add(a, b)))
+
 
 if __name__ == '__main__':
     pass
