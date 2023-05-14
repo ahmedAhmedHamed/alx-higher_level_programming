1cb7ee565a4cf66a99fc5f001f300a3470547d05
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-05-14 20:27:49 +0300

    (6)

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
     for i in matrix:
+        if not i:
+            continue
         for j in i:
             print("{:d}".format(j), end=" ")
         print("")
