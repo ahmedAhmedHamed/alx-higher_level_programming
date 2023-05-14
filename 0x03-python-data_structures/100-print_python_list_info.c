094061de0d37d2054abea0b8b68b81a7f44b930d
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-05-15 01:23:50 +0300

    (14) proper casting

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -6,15 +6,16 @@
 void print_python_list_info(PyObject *p)
 {
 	int i = 0;
-	int s = Py_SIZE(p);/*list size*/
-	int e = PyList_Size(p);
+	PyListObject *casted = (PyListObject *) p;
+	int s = Py_SIZE(casted);/*list size*/
+	int e = PyList_Size(casted);
 	PyObject *tempObj;
 
 	printf("[*] Size of the Python List = %d\n", s);
 	printf("[*] Allocated = %d\n", e);
 	while (i < s)
 	{
-		tempObj = PyList_GET_ITEM((PyListObject *) p, i);
+		tempObj = PyList_GET_ITEM(casted, i);
 		printf("Element %d: %s\n", i, tempObj->ob_type->tp_name);
 		i++;
 	}
