880d6872fdc9e393dee627a2c849274f61eeadbe
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-05-15 01:19:04 +0300

    made py version 3.4 (14)

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,6 @@
-#include "/usr/include/python3.10/Python.h"
-#include "/usr/include/python3.10/listobject.h"
-#include "/usr/include/python3.10/object.h"
+#include "/usr/include/python3.4/Python.h"
+#include "/usr/include/python3.4/listobject.h"
+#include "/usr/include/python3.4/object.h"
 #include <stdio.h>
 
 void print_python_list_info(PyObject *p)
