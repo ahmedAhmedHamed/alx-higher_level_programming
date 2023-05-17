7d620abac9e6e54985102e1284a64d0e985d5a40
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-05-17 21:56:55 +0300

    (8)

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    new_dictionary = a_dictionary
+    if key in new_dictionary:
+        del new_dictionary[key]
+    return new_dictionary
