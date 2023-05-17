6db6ad19e90cb304a32300ce81784ab6a3e559d2
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-05-17 23:59:14 +0300

    (12)

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    flag = 0
     number = 0
-    for i in range(len(roman_string) - 1, -1, -1):
+    i = len(roman_string) - 1
+    while i >= 0:
         if roman_string[i] == 'I':
             number += 1
-            if flag == 1:
-                number -= 2
         elif roman_string[i] == 'V':
             number += 5
-            flag = 1
+            if i - 1 >= 0 and roman_string[i - 1] == 'I':
+                number -= 1
         elif roman_string[i] == 'X':
             number += 10
-            flag = 1
+            if i - 1 >= 0 and roman_string[i - 1] == 'V':
+                number -= 5
         elif roman_string[i] == 'L':
             number += 50
-            flag = 1
+            if i - 1 >= 0 and roman_string[i - 1] == 'X':
+                number -= 10
         elif roman_string[i] == 'C':
             number += 100
-            flag = 1
+            if i - 1 >= 0 and roman_string[i - 1] == 'L':
+                number -= 50
         elif roman_string[i] == 'D':
             number += 500
-            flag = 1
+            if i - 1 >= 0 and roman_string[i - 1] == 'C':
+                number -= 100
         elif roman_string[i] == 'M':
             number += 1000
-            flag = 1
+            if i - 1 >= 0 and roman_string[i - 1] == 'D':
+                number -= 500
+        i -= 1
     return number
