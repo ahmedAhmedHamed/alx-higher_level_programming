5563101ce5e66dd6062103b6cbadebf217142776
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-05-17 23:27:41 +0300

    (12)

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    flag = 0
+    number = 0
+    for i in range(len(roman_string) - 1, -1, -1):
+        if roman_string[i] == 'I':
+            number += 1
+            if flag == 1:
+                number -= 2
+        elif roman_string[i] == 'V':
+            number += 5
+            flag = 1
+        elif roman_string[i] == 'X':
+            number += 10
+            flag = 1
+        elif roman_string[i] == 'L':
+            number += 50
+            flag = 1
+        elif roman_string[i] == 'C':
+            number += 100
+            flag = 1
+        elif roman_string[i] == 'D':
+            number += 500
+            flag = 1
+        elif roman_string[i] == 'M':
+            number += 1000
+            flag = 1
+    return number
