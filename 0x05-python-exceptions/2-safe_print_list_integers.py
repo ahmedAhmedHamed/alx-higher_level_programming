3eeafd9859ce3eb6ead564659055e5b4a4197c40
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2023-05-29 23:51:56 +0300

    2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,10 +4,10 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         for i in range(x):
             if isinstance(my_list[i], int):
-                print(my_list[i], end="")
+                print("{:d}".format(my_list[i]), end="")
                 k = k + 1
     except:
+        pass
+    finally:
         print()
         return k
-    print()
-    return k
