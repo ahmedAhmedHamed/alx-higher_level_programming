11baa3f4d26c75e43ddfa67f8c0f28a6be2bcf6e
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2023-05-30 00:16:34 +0300

    3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    division_value = 0
+    division_value = None
     try:
         if b == 0:
             raise ValueError("divide by zero")
         division_value = a / b
     except ValueError:
-        pass
+        return None
     finally:
         print("Inside result: {}".format(division_value))
     return division_value
