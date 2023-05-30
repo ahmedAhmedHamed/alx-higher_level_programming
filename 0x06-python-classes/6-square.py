01bcb45c0c412d0040d1d2333c6752ae5a503e6a
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2023-05-31 01:45:40 +0300

    better use of pos[1]

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -50,9 +50,8 @@ class Square:
 
     def my_print(self):
         if self.__size == 0:
-            if not self.__position[1] > 0:
-                for k in range(self.__position[0]):
-                    print(" ", end="")
+            print()
+        for i in range(self.__position[1]):
             print()
         for i in range(self.__size):
             for k in range(self.__position[0]):
