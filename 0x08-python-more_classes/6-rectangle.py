1c4b3ba8430b73b81e070f5417d5df243d9275c9
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2023-06-05 14:44:06 +0300

    Update 6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
     """ The aforementioned rectangle """
 
-    __number_of_instances = 0
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         self.__exception_raiser(width, "width")
