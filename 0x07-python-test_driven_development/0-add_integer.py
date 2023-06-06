33814cc4b772065a31562050a55f1fa618e43b19
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2023-06-06 05:51:13 +0300

    0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
-""" this module is for an add integer function """
+""" this module is for an add integer function
+
+    the function accepts two integers or two floats
+    or any combination of the above
+
+    floats will be converted to integers
+"""
 
 
 def add_integer(a, b=98):
+    """ adds two integers and allows floats
+
+    :arg a: the first number
+    :arg b: the second number
+    b has a default value, a does not
+    they are both going to be casted to floats
+    :returns
+        int: an integer as the result of the addition
+
+    """
     if isinstance(a, float):
         a = int(a)
     if isinstance(b, float):
