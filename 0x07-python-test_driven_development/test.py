5cdec02975dcbb263441cd1dac89e09f85e1ab77
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2023-06-08 17:02:42 +0300

    misc - all unnecessary

---
+++ 0x07-python-test_driven_development/test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+class hai():
+    def __init__(self):
+        self.__sup = 50
+
+haie = hai()
+
+print (haie.__sup)
---
+++ 0x08-python-more_classes/7-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+Rectangle = __import__('7-rectangle').Rectangle
+my_rectangle_3 = Rectangle(7, 3)
+print("--")
+my_rectangle_3.print_symbol = ["C", "is", "fun!"]
+print(my_rectangle_3)
+
+print("--")
