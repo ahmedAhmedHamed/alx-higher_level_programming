ae96eada38e5dce9f8ac9b7d823b6e812a857aad
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-06-12 16:05:32 +0300

    2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""this is the module for 2-is_same_class """
+
 
 def is_same_class(obj, a_class):
+    """function that checks if two classes are the same
+        (class type)
+    """
     return isinstance(obj, a_class)
