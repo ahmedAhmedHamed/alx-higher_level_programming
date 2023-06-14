030b5cfa8658d6d5f13889f496f9289d1070ee75
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-06-14 03:21:28 +0300

    0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module that reads a file"""
+
+
+def read_file(filename=""):
+    """reads a file called filename"""
+    with open(filename, 'r', encoding="utf-8") as file:
+        for line in file:
+            print(line)
