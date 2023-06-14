7bbc0e564e79d563a9bbbf01e47772f38bbf5d04
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-06-14 04:01:07 +0300

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -15,4 +15,4 @@ if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
     for i in json_list:
-        save_to_json_file("add_item.json", i)
+        save_to_json_file(i, "add_item.json")
