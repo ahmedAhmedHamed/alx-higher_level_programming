0b0ccab0a7d6f656b88543206c1052490b6c4dd5
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-06-14 04:08:52 +0300

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,13 +5,21 @@ a Python list, and then save them to a file
 """
 import json
 import sys
+from os import path
 
 if __name__ == "__main__":
     """the main driver of the file"""
     argv_split = sys.argv[1:]
     json_list = []
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+    if path.exists('add_item.json'):
+        json_list = load_from_json_file('add_item.json')
+    else:
+        json_list = []
+
     for count, value in argv_split:
         json_list.append(argv_split)
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
     save_to_json_file(json_list, "add_item.json")
