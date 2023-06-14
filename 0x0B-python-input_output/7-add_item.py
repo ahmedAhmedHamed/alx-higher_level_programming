104d6d6ea75447eaf421149e5d79afeecbb77d89
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-06-14 04:11:53 +0300

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,14 +3,12 @@
 a script that adds all arguments to
 a Python list, and then save them to a file
 """
-import json
 import sys
 from os import path
 
 if __name__ == "__main__":
     """the main driver of the file"""
     argv_split = sys.argv[1:]
-    json_list = []
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
@@ -19,7 +17,7 @@ if __name__ == "__main__":
     else:
         json_list = []
 
-    for count, value in argv_split:
+    for value in argv_split:
         json_list.append(argv_split)
 
     save_to_json_file(json_list, "add_item.json")
