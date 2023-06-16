5c063b44614cefca604243fe5b9b009b0abdb0e9
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-06-16 12:02:20 +0300

    added dictionary to json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """module for 'Base' class"""
+import json
 
 
 class Base:
@@ -18,3 +19,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """transforms the list_dictionaries to a json"""
+        if list_dictionaries is None or list_dictionaries.__len__() == 0:
+            return '"[]"'
+        return json.dumps(list_dictionaries)
