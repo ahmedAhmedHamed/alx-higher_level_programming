02689129fb521b20aa0de2c936a6c2581586c31d
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-08-14 21:04:02 +0300

    semistandard

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,9 +2,8 @@
 import { argv } from 'node:process';
 
 function factorial (n) {
-  if (isNaN(n) || n === 1)
-    return 1;
-  return n * factorial(n-1);
+  if (isNaN(n) || n === 1) { return 1; }
+  return n * factorial(n - 1);
 }
 
 console.log(factorial(argv[2]));
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 import { argv } from 'node:process';
-function add(a, b) {
+function add (a, b) {
   return a + b;
 }
 
 if (!isNaN(argv[2] && !isNaN(argv[3]))) {
   console.log(add(argv[2], argv[3]));
 } else {
-  console.log('NaN')
+  console.log('NaN');
 }
