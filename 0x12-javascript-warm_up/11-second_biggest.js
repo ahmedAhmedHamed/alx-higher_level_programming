29c2c103b363749a4869f6a890deaabd2b1d5351
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-08-14 21:45:47 +0300

    11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,13 +2,15 @@
 import { argv } from 'node:process';
 
 if (!argv[2]) { console.log(0); } else if (argv[2] && !argv[3]) { console.log(1); } else {
-  let smallest = 2;
-  let secondSmallest = 2;
-  for (let i = 2; i < argv.length - 1; i++) {
-    if (argv[smallest] < argv[i]) {
-      secondSmallest = smallest;
-      smallest = i;
+  let biggest = 2;
+  let secondBiggest = 2;
+  for (let i = 2; i < argv.length; i++) {
+    if (argv[biggest] < argv[i]) {
+      secondBiggest = biggest;
+      biggest = i;
+    } else if (argv[secondBiggest] < argv[i]) {
+      secondBiggest = i;
     }
   }
-  console.log(argv[secondSmallest]);
+  console.log(argv[secondBiggest]);
 }
