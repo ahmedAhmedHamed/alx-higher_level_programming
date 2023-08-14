4f7109788c1d2ae7eb4a1786c2dcbfb88e8b2203
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-08-14 21:47:17 +0300

    11 adjusted

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 import { argv } from 'node:process';
 
-if (!argv[2]) { console.log(0); } else if (argv[2] && !argv[3]) { console.log(1); } else {
+if (!argv[2]) {
+  console.log(0);
+} else if (argv[2] && !argv[3]) {
+  console.log(0);
+} else {
   let biggest = 2;
   let secondBiggest = 2;
   for (let i = 2; i < argv.length; i++) {
