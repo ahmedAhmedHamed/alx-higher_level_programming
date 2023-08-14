254c85e5fac573c8e41da82644ccde5bc48ecd88
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-08-14 19:54:48 +0300

    8 semistandard

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,9 +4,9 @@ import { argv } from 'node:process';
 if (isNaN(argv[2])) {
   console.log('Missing size');
 } else {
-  let line = ''
+  let line = '';
   for (let i = 0; i < argv[2]; i++) {
-    line += 'X'
+    line += 'X';
   }
   for (let j = 0; j < argv[2]; j++) {
     console.log(line);
