7b562e0a821882f92f1470275226344e2fed68f1
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-08-15 03:05:21 +0300

    6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
 const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < languages.length; i++) {
+for (let i = 0; i < languages.length; i++)
   console.log(languages[i]);
-}
