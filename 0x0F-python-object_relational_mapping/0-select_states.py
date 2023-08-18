2ec1cdf4c3177f17d92588298920280d49d670b2
Ahmed Hamed <ahmedahmedhamedahmed0@gmail.com>
2023-08-18 09:35:44 +0300

    0-select_states.py completed

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
-
+"""
+this module houses 0-select_states' solution
+"""
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
+    """
+    connects to a database on localhost:3306
+    and fetchest the names of all the states
+    """
     arguments = argv
     username = argv[1]
     password = argv[2]
     db_name = argv[3]
-    db = MySQLdb.connect(host='http://localhost:3306', user=username, passwd=password, db=db_name)
+    db = MySQLdb.connect(host='localhost',
+                         port=3306, user=username, passwd=password, db=db_name)
     cur = db.cursor()
-    cur.execute("SELECT states FROM hbtn_0e_0_usa")
-
+    cur.execute("SELECT name FROM states")
+    rows = cur.fetchall()
+    for index, row in enumerate(rows):
+        print(f"({index}, {row[0]})")
+    cur.close()
+    db.close()
