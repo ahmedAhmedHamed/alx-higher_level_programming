2809f789ea959eeb09c9262eca762bdc27030e1e
Ahmed Hamed <ahmedahmedhamedahmed0@gmail.com>
2023-08-18 19:52:31 +0300

    testing requirement (8)

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -5,7 +5,7 @@ this module houses the model_state class
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from sys import argv
-from model_state import Base, State
+# from model_state import Base, State
 
 if __name__ == "__main__":
     """
@@ -17,8 +17,9 @@ if __name__ == "__main__":
     db_uri = f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}"
     engine = create_engine(db_uri)
     with Session(engine) as session:
-        result = session.query(State).order_by(State.id).limit(1)
+        pass
+        # result = session.query(State).order_by(State.id).limit(1)
 
-        print(f"{result.all()[0].id}: {result.all()[0].name}")
-        if len(result.all()) == 0:
-            print("Nothing")
+        # print(f"{result.all()[0].id}: {result.all()[0].name}")
+        # if len(result.all()) == 0:
+        #     print("Nothing")
