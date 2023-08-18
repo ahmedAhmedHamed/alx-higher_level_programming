bf8b67e91c3a71eeb215070fccf05204a79d86c3
Ahmed Hamed <ahmedahmedhamedahmed0@gmail.com>
2023-08-18 15:01:23 +0300

    5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+this module houses 1-filter_states.py's solution
+"""
+import MySQLdb
+from sys import argv
+import re
+
+if __name__ == "__main__":
+    """
+    connects to a database on localhost:3306
+    and fetchest the names of all the states
+    """
+    arguments = argv
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    state_name_searched = argv[4]
+    db = MySQLdb.connect(host='localhost',
+                         port=3306, user=username, passwd=password, db=db_name)
+    cur = db.cursor()
+    state_name_searched = state_name_searched.split(';', 1)[0]
+    query = ('SELECT cities.name FROM states, cities'
+             ' WHERE BINARY states.name = "{0}"'
+             'AND states.id = cities.state_id'
+             ' ORDER BY cities.id ASC').format(state_name_searched)
+    cur.execute(query)
+    rows = cur.fetchall()
+    for index, row in enumerate(rows):
+        if index == len(rows) - 1:
+            print(row[0])
+        else:
+            print(row[0], end=', ')
+    cur.close()
+    db.close()
