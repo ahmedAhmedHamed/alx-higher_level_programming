9087700cd8e3ccfa6749684d92b07ac6ec03d8c8
Ahmed Hamed <ahmedahmedhamedahmed0@gmail.com>
2023-08-18 19:45:45 +0300

    moved imports inside main

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -2,13 +2,14 @@
 """
 this module houses the model_state class
 """
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from sqlalchemy import select
-from sys import argv
-from model_state import Base, State
+
 
 if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from sys import argv
+    from model_state import Base, State
+
     """
     root password hbtn_0e_0_usa
     """
@@ -18,7 +19,6 @@ if __name__ == "__main__":
     db_uri = f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}"
     engine = create_engine(db_uri)
     with Session(engine) as session:
-        # stmt = select(State).limit(1)
         result = session.query(State).limit(1)
 
         print(f"{result.all()[0].id}: {result.all()[0].name}")
