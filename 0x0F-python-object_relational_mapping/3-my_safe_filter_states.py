9d87e9babcd0ce5c160ce59c7fa940c2e10d9d27
Ahmed Hamed <ahmedahmedhamedahmed0@gmail.com>
2023-08-18 14:43:19 +0300

    (3)

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+this module houses 1-filter_states.py's solution
+"""
+import MySQLdb
+from sys import argv
+import re
+
+if __name__ == "__main__":
+    """
+    connects to a database on localhost:3306
+    and fetchest the names of all the states
+    """
+    arguments = argv
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    state_name_searched = argv[4]
+    db = MySQLdb.connect(host='localhost',
+                         port=3306, user=username, passwd=password, db=db_name)
+    cur = db.cursor()
+    state_name_searched = state_name_searched.split(';', 1)[0]
+    query = ('SELECT id, name FROM states WHERE BINARY name = "{0}"'
+             ' ORDER BY id ASC').format(state_name_searched)
+    cur.execute(query)
+    rows = cur.fetchall()
+    for index, row in enumerate(rows):
+        print(row)
+    cur.close()
+    db.close()
