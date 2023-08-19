7061b75a8b992804a484840879f292b33805b6f1
Ahmed Hamed <ahmedahmedhamedahmed0@gmail.com>
2023-08-19 22:17:29 +0300

    8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -5,21 +5,21 @@ this module houses the model_state class
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from sys import argv
-# from model_state import Base, State
+from model_state import Base, State
 
 if __name__ == "__main__":
     """
     root password hbtn_0e_0_usa
     """
-    username = argv[1]
-    password = argv[2]
-    db_name = argv[3]
-    db_uri = f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}"
-    engine = create_engine(db_uri)
-    with Session(engine) as session:
-        pass
-        # result = session.query(State).order_by(State.id).limit(1)
-
-        # print(f"{result.all()[0].id}: {result.all()[0].name}")
-        # if len(result.all()) == 0:
-        #     print("Nothing")
+    pass
+    # username = argv[1]
+    # password = argv[2]
+    # db_name = argv[3]
+    # db_uri =f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}"
+    # engine = create_engine(db_uri)
+    # with Session(engine) as session:
+    #     result = session.query(State).order_by(State.id).limit(1)
+    #
+    #     print(f"{result.all()[0].id}: {result.all()[0].name}")
+    #     if len(result.all()) == 0:
+    #         print("Nothing")
