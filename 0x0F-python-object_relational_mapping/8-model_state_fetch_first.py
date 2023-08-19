2c48393817805058b72a87878e91d39b711d9a96
Ahmed Hamed <ahmedahmedhamedahmed0@gmail.com>
2023-08-19 22:21:07 +0300

    8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -2,24 +2,25 @@
 """
 this module houses the model_state class
 """
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import Session
-# from sys import argv
-# from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sys import argv
+from model_state import Base, State
 
 if __name__ == "__main__":
     """
     root password hbtn_0e_0_usa
     """
-    pass
-    # username = argv[1]
-    # password = argv[2]
-    # db_name = argv[3]
-    # db_uri =f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}"
-    # engine = create_engine(db_uri)
-    # with Session(engine) as session:
-    #     result = session.query(State).order_by(State.id).limit(1)
-    #
-    #     print(f"{result.all()[0].id}: {result.all()[0].name}")
-    #     if len(result.all()) == 0:
-    #         print("Nothing")
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    db_uri = f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}"
+    engine = create_engine(db_uri)
+    with Session(engine) as session:
+        result = session.query(State).order_by(State.id).first()
+
+        if result is not None:
+            print(f"{result.id}: {result.name}")
+        else:
+            print('Nothing')
+
