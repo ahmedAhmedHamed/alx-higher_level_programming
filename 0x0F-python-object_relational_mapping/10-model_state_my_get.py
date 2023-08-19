3a128f0996a18991945d77b7f0ef407a4f30e5b3
Ahmed Hamed <ahmedahmedhamedahmed0@gmail.com>
2023-08-19 22:49:25 +0300

    backing up 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+this module houses the model_state class
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sys import argv
+from model_state import Base, State
+
+if __name__ == "__main__":
+    """
+    root password hbtn_0e_0_usa
+    """
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    state_name_searched = argv[4]
+    safe_state_name_searched = state_name_searched.split(';', 1)[0]
+    db_uri = f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}"
+    engine = create_engine(db_uri)
+    with Session(engine) as session:
+        results = session.query(State
+                                ).where(
+            State.name.any == safe_state_name_searched)
+
+        for result in results:
+            print(f"{result.id}: {result.name}")
