2c2bd9bcd945fdf520d4a8d5b57533dfc767767a
Ahmed Hamed <ahmedahmedhamedahmed0@gmail.com>
2023-08-20 05:10:46 +0300

    12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+this module houses the model_state class
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sys import argv
+from model_state import Base, State
+
+if __name__ == "__main__":
+    """
+    root password hbtn_0e_0_usa
+    Change the name of the State where id = 2 to New Mexico
+    """
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    db_uri = f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}"
+    engine = create_engine(db_uri)
+    with Session(bind=engine) as session:
+        (session.query(State).
+         filter(State.id == 2).
+         update({"name": "new_mexico"})
+         )
+        session.commit()
