c2371e4f121aaf9fa1a76c6021440424cd2eed4a
Ahmed Hamed <ahmedahmedhamedahmed0@gmail.com>
2023-08-20 05:52:12 +0300

    14

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+this module houses the model_state class
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from sys import argv
+from model_state import Base, State
+from model_city import City
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    db_uri = f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}"
+    engine = create_engine(db_uri)
+    with Session(engine) as session:
+        query = session.query(City, State).join(State)
+        for city, state in query.all():
+            print(f"{state.name}: ({city.id}) {city.name}")
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+this module houses the model_state class
+"""
+
+from model_state import Base, State
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
+
+class City(Base):
+    """state class, same as in table"""
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = ForeignKey(Integer, ForeignKey("states.id"))
