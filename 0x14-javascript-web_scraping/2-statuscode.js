eb3ca762e055f838559ad9b5a289cd231a610457
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-07-31 00:18:24 +0300

    2 whitespace

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,7 @@ const requestModule = require('request');
 
 try {
   requestModule(process.argv[2], (error, response) => {
-    console.log('code: ', response.statusCode);
+    console.log('code:', response.statusCode);
     if (error) console.error(error);
   });
 } catch (e) {
