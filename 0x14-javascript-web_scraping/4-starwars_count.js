dc9ff2c4cbe8438e6c59788cd53036843151e59f
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-07-31 00:21:44 +0300

    4 semistandard

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,25 +1,25 @@
 #!/usr/bin/node
-const request_module = require('request');
+const requestModule = require('request');
 
 try {
-    let request_url = process.argv[2];
-    request_module(request_url, (error, response) => {
-        let movies = JSON.parse(response.body)['results'];
-        let character_appearance_counter = 0;
-        let searched_character = "18";
-        movies.forEach(movie => {
-            movie['characters'].forEach(character_url => {
-                const trimmedUrl = character_url.endsWith('/')
-                    ? character_url.slice(0, -1) : character_url;
-                const parts = trimmedUrl.split('/');
-                const number = parts[parts.length - 1];
-                if (number === searched_character)
-                    ++character_appearance_counter;
-            })
-        });
-        console.log(character_appearance_counter);
+  const requestUrl = process.argv[2];
+  requestModule(requestUrl, (error, response) => {
+    if (error) console.error(error);
+    const movies = JSON.parse(response.body).results;
+    let characterAppearanceCounter = 0;
+    const searchedCharacter = '18';
+    movies.forEach(movie => {
+      movie.characters.forEach(characterUrl => {
+        const trimmedUrl = characterUrl.endsWith('/')
+          ? characterUrl.slice(0, -1)
+          : characterUrl;
+        const parts = trimmedUrl.split('/');
+        const number = parts[parts.length - 1];
+        if (number === searchedCharacter) { ++characterAppearanceCounter; }
+      });
     });
-}
-catch (e) {
+    console.log(characterAppearanceCounter);
+  });
+} catch (e) {
   console.error(e);
 }
