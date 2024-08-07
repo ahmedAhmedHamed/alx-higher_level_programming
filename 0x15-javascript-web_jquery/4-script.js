71084db0f979f5b43a17da0cf839f0c7c8e763bc
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-08-07 17:59:26 +0300

    (4)

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,12 @@
+$('DIV#toggle_header').click(function () {
+  const header = $('header');
+  if (header.hasClass('green')) {
+      header.removeClass('green');
+      header.addClass('red');
+  } else if (header.hasClass('red')) {
+      header.removeClass('red');
+      header.addClass('green');
+  } else {
+      header.addClass('red');
+  }
+});
